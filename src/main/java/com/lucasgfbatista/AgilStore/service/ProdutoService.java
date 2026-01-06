ddf28317aca7eb908332f53efd7e21bd24ba631d
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 12:00:21 -0300

    feat(service): method to update by id

--- src/main/java/com/lucasgfbatista/AgilStore/service/ProdutoService.java
+++ src/main/java/com/lucasgfbatista/AgilStore/service/ProdutoService.java
@@ -3,10 +3,12 @@ package com.lucasgfbatista.AgilStore.service;
 import com.lucasgfbatista.AgilStore.domain.Produto;
 import com.lucasgfbatista.AgilStore.dto.ProdutoRequestDTO;
 import com.lucasgfbatista.AgilStore.dto.ProdutoResponseDTO;
+import com.lucasgfbatista.AgilStore.exception.ResourceNotFoundException;
 import com.lucasgfbatista.AgilStore.mapper.ProdutoMapper;
 import com.lucasgfbatista.AgilStore.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
 
 @Service
@@ -24,7 +26,7 @@ public class ProdutoService {
 
     - [x] - Criar
     - [x] - Lista todos
-    - [ ] - Atualizar por id
+    - [x] - Atualizar por id
     - [ ] - Deletar por id
     - [ ] - Buscar por id
     - [ ] - Buscar por nome
@@ -45,5 +47,22 @@ public class ProdutoService {
                 .stream()
                 .map(produtoMapper::toResponse)
                 .toList();
+    }
+
+    public ProdutoResponseDTO atualiarProduto(Long id, ProdutoRequestDTO dto) {
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("Produto", "id", id)
+                );
+
+        produto.setNome(dto.nome());
+        produto.setQntEstoque(dto.qntEstoque());
+        produto.setPreco(dto.preco());
+        produto.setCategoria(produtoMapper.toEntity(dto).getCategoria());
+
+        produtoRepository.save(produto);
+
+        return produtoMapper.toResponse(produto);
+
     }
 }
