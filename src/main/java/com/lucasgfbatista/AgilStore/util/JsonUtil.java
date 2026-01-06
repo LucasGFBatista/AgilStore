b9fcf46fb8e1da8bfffc0afc3aa41a018f881618
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 15:48:47 -0300

    fix(util): correct to create path file if not exists

--- src/main/java/com/lucasgfbatista/AgilStore/util/JsonUtil.java
+++ src/main/java/com/lucasgfbatista/AgilStore/util/JsonUtil.java
@@ -1,11 +1,9 @@
 package com.lucasgfbatista.AgilStore.util;
 
-
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-
 
 public class JsonUtil {
 
@@ -13,22 +11,23 @@ public class JsonUtil {
 
     public static <T> void salvar(String caminhoArquivo, T objeto) {
         try {
-            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(caminhoArquivo), objeto);
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao salvar JSON: " + e.getMessage(), e);
-        }
-    }
-
-    public static <T> T ler(String caminhoArquivo, Class<T> classe){
-        try {
             File arquivo = new File(caminhoArquivo);
 
-            if(!arquivo.exists()){
-                return null;
+
+            File pasta = arquivo.getParentFile();
+            if (pasta != null && !pasta.exists()) {
+                pasta.mkdirs();
             }
-            return mapper.readValue(arquivo, classe);
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao ler JSON: " + e.getMessage(), e);
+
+            mapper
+                    .writerWithDefaultPrettyPrinter()
+                    .writeValue(arquivo, objeto);
+
+        } catch (IOException e) {
+            throw new RuntimeException(
+                    "Erro ao salvar JSON: " + caminhoArquivo,
+                    e
+            );
         }
     }
 }
