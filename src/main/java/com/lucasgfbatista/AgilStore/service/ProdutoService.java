2289dbe49b691ea8ec77fce6a1664f63af1eef59
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 11:51:00 -0300

    feat(service): method to create

--- src/main/java/com/lucasgfbatista/AgilStore/service/ProdutoService.java
+++ src/main/java/com/lucasgfbatista/AgilStore/service/ProdutoService.java
@@ -1,4 +1,42 @@
 package com.lucasgfbatista.AgilStore.service;
 
+import com.lucasgfbatista.AgilStore.domain.Produto;
+import com.lucasgfbatista.AgilStore.dto.ProdutoRequestDTO;
+import com.lucasgfbatista.AgilStore.dto.ProdutoResponseDTO;
+import com.lucasgfbatista.AgilStore.mapper.ProdutoMapper;
+import com.lucasgfbatista.AgilStore.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+    private final ProdutoMapper produtoMapper;
+
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoMapper produtoMapper) {
+        this.produtoRepository = produtoRepository;
+        this.produtoMapper = produtoMapper;
+    }
+
+    /* todo
+
+    - [x] - Criar
+    - [ ] - Lista todos
+    - [ ] - Atualizar por id
+    - [ ] - Deletar por id
+    - [ ] - Buscar por id
+    - [ ] - Buscar por nome
+    * */
+
+
+    public ProdutoResponseDTO criarProduto(ProdutoRequestDTO dto) {
+
+        Produto produto = produtoMapper.toEntity(dto);
+
+        produtoRepository.save(produto);
+
+        return produtoMapper.toResponse(produto);
+    }
+
+    
 }
