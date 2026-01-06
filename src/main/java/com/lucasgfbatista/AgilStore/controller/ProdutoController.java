24b8cb9a9713b0f1a63eb3f6c92869a1ac47f74a
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 12:29:31 -0300

    feat(controller): added dependency injection

--- src/main/java/com/lucasgfbatista/AgilStore/controller/ProdutoController.java
+++ src/main/java/com/lucasgfbatista/AgilStore/controller/ProdutoController.java
@@ -1,5 +1,16 @@
 package com.lucasgfbatista.AgilStore.controller;
 
+import com.lucasgfbatista.AgilStore.service.ProdutoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
 }
