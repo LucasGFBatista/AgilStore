12493d20b2a67315111af0bb311538683439115d
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 11:07:54 -0300

    chore(repository): added annotation

--- src/main/java/com/lucasgfbatista/AgilStore/repository/CategoriaRepository.java
+++ src/main/java/com/lucasgfbatista/AgilStore/repository/CategoriaRepository.java
@@ -2,9 +2,12 @@ package com.lucasgfbatista.AgilStore.repository;
 
 import com.lucasgfbatista.AgilStore.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
     Optional<Categoria> findByNomeIgnoreCase();
