f587505bce88fb16d7f760e72f69ad8a4b96744b
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 13:13:22 -0300

    docs(controller): added method GET - find by id and find by id

--- src/main/java/com/lucasgfbatista/AgilStore/controller/ProdutoController.java
+++ src/main/java/com/lucasgfbatista/AgilStore/controller/ProdutoController.java
@@ -19,9 +19,9 @@ public class ProdutoController {
 
     /* todo
      * - [x] - POST - CRIAR
-     * - [ ] - GET - LISTAR TODOS
-     * - [ ] - GET - BUSCAR POR ID
-     * - [ ] - GET - BUSCAR POR NOME
+     * - [x] - GET - LISTAR TODOS
+     * - [x] - GET - BUSCAR POR ID
+     * - [x] - GET - BUSCAR POR NOME
      * - [ ] - PUT - ATUALIZAR POR ID
      * - [ ] - DELETE - DELETAR POR ID
      * */
@@ -37,5 +37,14 @@ public class ProdutoController {
         return produtoService.listarTodosProdutos();
     }
 
+    @GetMapping("/{id}")
+    public ProdutoResponseDTO buscarProduto(@PathVariable Long id){
+        return produtoService.buscarProduto(id);
+    }
+
+    @GetMapping("/{nome")
+    public ProdutoResponseDTO buscarProduto(@PathVariable String nome{
+        return produtoService.buscarProduto(nome);
+    }
 
 }
