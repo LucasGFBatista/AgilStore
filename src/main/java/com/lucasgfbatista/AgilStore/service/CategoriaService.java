036798548b20f62e00a9a685afb7534e1ca3f84f
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 12:08:54 -0300

    docs(to-do): added to-do list

--- src/main/java/com/lucasgfbatista/AgilStore/service/CategoriaService.java
+++ src/main/java/com/lucasgfbatista/AgilStore/service/CategoriaService.java
@@ -1,4 +1,18 @@
 package com.lucasgfbatista.AgilStore.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CategoriaService {
+
+        /* todo
+
+    - [ ] - Criar
+    - [ ] - Lista todos
+    - [ ] - Atualizar por id
+    - [ ] - Deletar por id
+    - [ ] - Buscar por id
+    - [ ] - Buscar por nome
+    * */
+
 }
