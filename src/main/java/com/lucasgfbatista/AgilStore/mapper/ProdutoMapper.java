5a97d18cbfa87b36a1f5d54ddb5c663fbe9fbb38
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 11:03:48 -0300

    feat(mapper): created mapper of produto

--- src/main/java/com/lucasgfbatista/AgilStore/mapper/ProdutoMapper.java
+++ src/main/java/com/lucasgfbatista/AgilStore/mapper/ProdutoMapper.java
@@ -1,4 +1,52 @@
 package com.lucasgfbatista.AgilStore.mapper;
 
+import com.lucasgfbatista.AgilStore.domain.Categoria;
+import com.lucasgfbatista.AgilStore.domain.Produto;
+import com.lucasgfbatista.AgilStore.dto.ProdutoRequestDTO;
+import com.lucasgfbatista.AgilStore.dto.ProdutoResponseDTO;
+import com.lucasgfbatista.AgilStore.exception.ResourceNotFoundException;
+import com.lucasgfbatista.AgilStore.repository.CategoriaRepository;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProdutoMapper {
+
+    private final CategoriaRepository categoriaRepository;
+
+    public ProdutoMapper(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public Produto toEntity(ProdutoRequestDTO dto) {
+        Produto produto = new Produto();
+
+        produto.setNome(dto.nome());
+
+        Categoria categoria = categoriaRepository.findById(dto.categoriaId())
+                .orElseThrow(
+                        () -> new ResourceNotFoundException(
+                                "Categoria",
+                                "id",
+                                dto.categoriaId())
+                );
+
+        produto.setCategoria(categoria);
+        produto.setQntEstoque(dto.qntEstoque());
+        produto.setPreco(dto.preco());
+
+
+        return produto;
+    }
+
+
+    public ProdutoResponseDTO toResponse(Produto produto) {
+        return new ProdutoResponseDTO(
+                produto.getId(),
+                produto.getNome(),
+                produto.getCategoria() != null ? produto.getCategoria().getId() : null,
+                produto.getQntEstoque(),
+                produto.getPreco()
+
+        );
+    }
 }
