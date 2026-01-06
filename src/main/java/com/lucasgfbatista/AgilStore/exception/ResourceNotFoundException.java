c596441ebf37e12f4dd4fe639337254d340e615a
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 11:01:05 -0300

    feat(exception): created resourceNotFoundExcepetion to handle those kind exception as well

--- src/main/java/com/lucasgfbatista/AgilStore/exception/ResourceNotFoundException.java
+++ src/main/java/com/lucasgfbatista/AgilStore/exception/ResourceNotFoundException.java
@@ -1,7 +1,43 @@
 package com.lucasgfbatista.AgilStore.exception;
 
+
 public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
+
+    private final String recurso;
+    private final String campo;
+    private final Object valor;
+
+    public ResourceNotFoundException(
+            String recurso,
+            String campo,
+            Object valor
+    ) {
+        super(recurso + " não encontradeo com " + campo + ": " + valor);
+        this.recurso = recurso;
+        this.campo = campo;
+        this.valor = valor;
+    }
+
+    public ResourceNotFoundException(String mensagem, String recurso, String campo, Object valor) {
+        super(mensagem);
+        this.recurso = recurso;
+        this.campo = campo;
+        this.valor = valor;
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable cause, String recurso, String campo, Object valor) {
+        super(mensagem, cause);
+        this.recurso = recurso;
+        this.campo = campo;
+        this.valor = valor;
+    }
+
+    public ResourceNotFoundException(Throwable cause, String recurso, String campo, Object valor) {
+        super(cause);
+        this.recurso = recurso;
+        this.campo = campo;
+        this.valor = valor;
+    }
+
+
 }
