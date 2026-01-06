5a8e04b25ebff2de148423b2faf13548628dd2fb
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 13:35:58 -0300

    feat(util): added method to list itens from json

--- src/main/java/com/lucasgfbatista/AgilStore/util/ProdutoJsonUtil.java
+++ src/main/java/com/lucasgfbatista/AgilStore/util/ProdutoJsonUtil.java
@@ -1,7 +1,7 @@
 package com.lucasgfbatista.AgilStore.util;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lucasgfbatista.AgilStore.domain.Produto;
-import tools.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,8 +11,8 @@ import java.util.List;
 public class ProdutoJsonUtil {
 
     private static final ObjectMapper mapper = new ObjectMapper();
-    private static final File arquivo = new File("produto.json");
-
+    private static final File arquivo = new File("produtos.json");
+    
     public static void salvarEmJson(List<Produto> produtos) {
         try {
             mapper.writerWithDefaultPrettyPrinter().writeValue(arquivo, produtos);
@@ -21,17 +21,15 @@ public class ProdutoJsonUtil {
         }
     }
 
+
     public static List<Produto> carregarDoJson() {
         try {
             if (!arquivo.exists()) return List.of();
             Produto[] produtos = mapper.readValue(arquivo, Produto[].class);
-
             return Arrays.asList(produtos);
         } catch (IOException e) {
             e.printStackTrace();
-
             return List.of();
         }
     }
-
 }
