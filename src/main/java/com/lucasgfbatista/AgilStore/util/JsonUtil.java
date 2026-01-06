f62988f906808f7bb49e87b202438522aa25e758
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 14:15:05 -0300

    feat(util): method to save on json

--- src/main/java/com/lucasgfbatista/AgilStore/util/JsonUtil.java
+++ src/main/java/com/lucasgfbatista/AgilStore/util/JsonUtil.java
@@ -1,7 +1,23 @@
 package com.lucasgfbatista.AgilStore.util;
 
 
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+
 public class JsonUtil {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static <T> void salvar(String caminhoArquivo, T objeto) {
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(new File(caminhoArquivo), objeto);
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao salvar JSON: " + e.getMessage(), e);
+        }
+    }
 
 
 }
