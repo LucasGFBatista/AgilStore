3cf02213d00c8f6beffe2610b5a33b9399f9c046
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 14:40:19 -0300

    feat(controller): added methods to create, list, update and delete

--- src/main/java/com/lucasgfbatista/AgilStore/controller/CategoriaController.java
+++ src/main/java/com/lucasgfbatista/AgilStore/controller/CategoriaController.java
@@ -1,9 +1,48 @@
 package com.lucasgfbatista.AgilStore.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lucasgfbatista.AgilStore.dto.CategoriaRequestDTO;
+import com.lucasgfbatista.AgilStore.dto.CategoriaResponseDTO;
+import com.lucasgfbatista.AgilStore.dto.CategoriaRequestDTO;
+import com.lucasgfbatista.AgilStore.dto.CategoriaResponseDTO;
+import com.lucasgfbatista.AgilStore.service.CategoriaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaController {
+
+    private final CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
+    @PostMapping
+    public CategoriaResponseDTO criarCategoria(@RequestParam CategoriaRequestDTO dto) {
+        return categoriaService.criarCategoria(dto);
+    }
+    
+    
+    @GetMapping(value = {"/", ""})
+    public List<CategoriaResponseDTO> listarCategorias() {
+        return categoriaService.listarTodosCategorias();
+    }
+
+
+
+    @PutMapping("/{id}")
+    public CategoriaResponseDTO atualizarCategoria(
+            @PathVariable Long id,
+            @RequestBody CategoriaRequestDTO dto) {
+
+        return categoriaService.atualizarCategoria(id, dto);
+
+    }
+    
+    @DeleteMapping("/{id}")
+    public void deletarCategoria(@PathVariable Long id) {
+        categoriaService.deletarCategoria(id);
+    }
 }
