1bc49b08dbb2566c793032ff37dd30d6d897a010
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 14:18:40 -0300

    feat(util): method to read json

--- src/main/java/com/lucasgfbatista/AgilStore/util/JsonUtil.java
+++ src/main/java/com/lucasgfbatista/AgilStore/util/JsonUtil.java
@@ -14,10 +14,21 @@ public class JsonUtil {
     public static <T> void salvar(String caminhoArquivo, T objeto) {
         try {
             mapper.writerWithDefaultPrettyPrinter().writeValue(new File(caminhoArquivo), objeto);
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new RuntimeException("Erro ao salvar JSON: " + e.getMessage(), e);
         }
     }
 
+    public static <T> T ler(String caminhoArquivo, Class<T> classe){
+        try {
+            File arquivo = new File(caminhoArquivo);
 
+            if(!arquivo.exists()){
+                return null;
+            }
+            return mapper.readValue(arquivo, classe);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao ler JSON: " + e.getMessage(), e);
+        }
+    }
 }
