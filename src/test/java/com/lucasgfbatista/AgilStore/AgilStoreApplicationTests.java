35092c9248d466d133ace7754bdb5f01a6ee7d3b
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 09:52:16 -0300

    firs commit

---
+++ src/test/java/com/lucasgfbatista/AgilStore/AgilStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgfbatista.AgilStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgilStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
