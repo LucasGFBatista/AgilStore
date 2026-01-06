bd4df015cabd4c54a2364faed2ee6d7dc8ad3c50
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-06 14:31:09 -0300

    feat(controller): create categorias controller

---
+++ src/main/java/com/lucasgfbatista/AgilStore/controller/CategoriaController.java
@@ -0,0 +1,9 @@
+package com.lucasgfbatista.AgilStore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+}
