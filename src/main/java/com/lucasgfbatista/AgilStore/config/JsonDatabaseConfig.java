66457ae1345aed817332631e4b94737766f686ed
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-07 16:58:15 -0300

    feat(util): added method to inialize Produto datas

--- src/main/java/com/lucasgfbatista/AgilStore/config/JsonDatabaseConfig.java
+++ src/main/java/com/lucasgfbatista/AgilStore/config/JsonDatabaseConfig.java
@@ -2,12 +2,16 @@ package com.lucasgfbatista.AgilStore.config;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.lucasgfbatista.AgilStore.domain.Categoria;
+import com.lucasgfbatista.AgilStore.domain.Produto;
 import com.lucasgfbatista.AgilStore.dto.CategoriaResponseDTO;
+import com.lucasgfbatista.AgilStore.dto.ProdutoResponseDTO;
 import com.lucasgfbatista.AgilStore.repository.CategoriaRepository;
+import com.lucasgfbatista.AgilStore.repository.ProdutoRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.lucasgfbatista.AgilStore.util.*;
+import org.springframework.context.annotation.DependsOn;
 
 import java.util.List;
 
@@ -22,8 +26,9 @@ public class JsonDatabaseConfig {
     ) {
         JsonStorageService<CategoriaResponseDTO> categoriaJsonStorage =
                 new JsonStorageServiceImpl<>(
-                        CAMINHO_ARQUIVO+"categorias.json",
-                        new TypeReference<>() {}
+                        CAMINHO_ARQUIVO + "categorias.json",
+                        new TypeReference<>() {
+                        }
                 );
 
         return new JsonToDatabaseInitializer<>(
@@ -38,6 +43,32 @@ public class JsonDatabaseConfig {
         );
     }
 
+    @Bean
+    @DependsOn("categoriaJsonLoader")
+    public JsonToDatabaseInitializer<ProdutoResponseDTO, Produto> produtoJsonLoader(
+            ProdutoRepository produtoRepository,
+            CategoriaRepository categoriaRepository
+    ) {
+        JsonStorageService<ProdutoResponseDTO> produtoJsonStorage =
+                new JsonStorageServiceImpl<>(
+                        CAMINHO_ARQUIVO+"produtos.json",
+                        new TypeReference<>() {
+                        }
+                );
 
+        return new JsonToDatabaseInitializer<>(
+                produtoJsonStorage,
+                produtoRepository,
+                dto -> {
+                    Produto p = new Produto();
+                    p.setNome(dto.nome());
+                    p.setQntEstoque(dto.qntEstoque());
+                    p.setPreco(dto.preco());
+                    p.setCategoria(categoriaRepository.findById(dto.categoriaId())
+                            .orElseThrow(() -> new RuntimeException("Categoria não encontrada")));
+                    return p;
+                }
+        );
+    }
 
 }
