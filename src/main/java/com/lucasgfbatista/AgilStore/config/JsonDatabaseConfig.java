37af95f91dae3c801b15771582259000e8c77530
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-07 15:21:36 -0300

    feat(util): added method to inialize Categorias datas

--- src/main/java/com/lucasgfbatista/AgilStore/config/JsonDatabaseConfig.java
+++ src/main/java/com/lucasgfbatista/AgilStore/config/JsonDatabaseConfig.java
@@ -14,6 +14,29 @@ import java.util.List;
 @Configuration
 public class JsonDatabaseConfig {
 
+    private static final String CAMINHO_ARQUIVO = "json_projeto/";
+
+    @Bean
+    public JsonToDatabaseInitializer<CategoriaResponseDTO, Categoria> categoriaJsonLoader(
+            CategoriaRepository categoriaRepository
+    ) {
+        JsonStorageService<CategoriaResponseDTO> categoriaJsonStorage =
+                new JsonStorageServiceImpl<>(
+                        CAMINHO_ARQUIVO+"categorias.json",
+                        new TypeReference<>() {}
+                );
+
+        return new JsonToDatabaseInitializer<>(
+                categoriaJsonStorage,
+                categoriaRepository,
+                dto -> {
+                    Categoria c = new Categoria();
+                    c.setId(dto.id());
+                    c.setNome(dto.nome());
+                    return c;
+                }
+        );
+    }
 
 
 
