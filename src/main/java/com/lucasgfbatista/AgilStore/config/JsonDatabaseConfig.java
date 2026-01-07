ade9086fa69fb7a3942374e3932ecca177787698
LucasGFBatista <lucas.gf.batista@gmail.com>
2026-01-07 15:21:00 -0300

    feat(util): create jsontoDataBaseInializer

--- src/main/java/com/lucasgfbatista/AgilStore/config/JsonDatabaseConfig.java
+++ src/main/java/com/lucasgfbatista/AgilStore/config/JsonDatabaseConfig.java
@@ -1,7 +1,20 @@
 package com.lucasgfbatista.AgilStore.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.lucasgfbatista.AgilStore.domain.Categoria;
+import com.lucasgfbatista.AgilStore.dto.CategoriaResponseDTO;
+import com.lucasgfbatista.AgilStore.repository.CategoriaRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.lucasgfbatista.AgilStore.util.*;
+
+import java.util.List;
 
 @Configuration
 public class JsonDatabaseConfig {
+
+
+
+
 }
